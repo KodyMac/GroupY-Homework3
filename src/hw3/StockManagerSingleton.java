85c4a3ef3759e0e0c7c67441f4b9d0db3e309181
Kody McNamara <Kody McNamara@10.7.15.193>
2024-02-11 14:41:58 -0600

    move location of inventory file and created singleton part of stockmanagersingleton

--- src/hw3/StockManagerSingleton.java
+++ src/hw3/StockManagerSingleton.java
@@ -1,5 +1,23 @@
 package hw3;
+import java.io.BufferedWriter; 
+import java.io.FileWriter;
+import java.io.IOException; 
+
 
 public class StockManagerSingleton {
-
+	private static StockManagerSingleton instance = null;
+	
+	private final static String logFilePath="files/inventoryGiven.csv";
+	
+	//private constructor prevents class being created outside of this (only be created by itself)
+	private StockManagerSingleton() {
+	}
+	
+	public static StockManagerSingleton getInstance() {
+		if(instance==null) {
+			instance = new StockManagerSingleton();
+		}
+		return instance;
+	}
+	
 }
